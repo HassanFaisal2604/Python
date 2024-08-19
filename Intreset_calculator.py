96ad940da6498a9d9c0acd873196d067d7e64b81
Hassan <22223180@ncbae.edu.pk>
2024-08-19 20:19:10 +0500

    New files

---
+++ Intreset_calculator.py
@@ -0,0 +1,6 @@
+Principal=int(1000)
+rate=5
+time=2
+Sipmle_intrest=float(Principal*rate*time)/100
+print("With a principal amount of" , Principal, "USD", "at an annual interest rate of " ,rate,"%", "in 2 years you will earn ",Sipmle_intrest,"USD in interest."
+)
---
+++ string_add.py
@@ -0,0 +1,3 @@
+name1="jack"
+name2="Rose"
+print(name1,"and",name2,"Were on titanic in",int(1912))
