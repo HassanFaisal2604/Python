5044a6f3a4eb6d7349159200c8691b64582765dd
Hassan <22223180@ncbae.edu.pk>
2024-08-19 15:56:08 +0500

    added new fiel

---
+++ avg_height.py
@@ -0,0 +1,6 @@
+as_height=1.70
+yoda_height=0.66 
+darth_height=2.00
+luke_height=1.72
+avg_height=float(as_height+yoda_height+darth_height+luke_height)/4
+print(avg_height)
--- str.py
+++ str.py
@@ -2,8 +2,3 @@ name = str("Dart wader")
 print (name)
 age = int(19)
 print (age)
-bool_example = bool(1)
-if age == 19:
-    print(bool_example)
-else:
-    print(False)
