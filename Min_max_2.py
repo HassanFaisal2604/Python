56c8ee1e4fe18bd19b618634ea7ca74b94392afe
Hassan <22223180@ncbae.edu.pk>
2024-08-20 01:13:59 +0500

    new

---
+++ Min_max_2.py
@@ -0,0 +1,5 @@
+Ash_height=1.70
+yoda=0.66
+r2d2=1.09
+avg_height=round(Ash_height+yoda+r2d2)/3
+print("THe avg height is",avg_height,"THe Max height is",max(Ash_height,yoda,r2d2),"THe min height is",min(Ash_height,yoda,r2d2))
