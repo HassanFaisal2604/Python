1a2f5a18f19c4a191d28eb96237fa742b1fb2714
Hassan <2223180@ncbae.edu.pk>
2024-08-21 14:06:14 -0700

    added ne Lines

--- function_2.py
+++ function_2.py
@@ -1,7 +1,8 @@
-def titanic():
-    name=input("Who was on titanic when it sunk")
-    year=int(input("What year is it today"))
+name=input("Who was on titanic when it sunk")
+year=int(input("What year is it today"))
+def titanic(name,year):
+    
     year_Passed=year-1912
     print(f" {name}  were on titanic when it sunk",year_Passed," years ago")
-titanic()
+titanic(name,year)
     
