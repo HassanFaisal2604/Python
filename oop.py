eeaa2fdbc63c312015b0320d24bab1d26cb8166f
global <2223180@ncbae.edu.pk>
2024-10-03 18:49:49 +0500

    New files

--- oop.py
+++ oop.py
@@ -1,6 +1,13 @@
-class Person():
-    def __init__(self,name,age):
-     self.age=age
-     self.name=name
-person1=Person("Hassan",20)
-print(person1.name,person1.age)
+class Table:
+    def __init__(self, name, quality):
+        self.name = name
+        self.quality = quality
+
+    def __str__(self):
+        return f"Table(name='{self.name}', quality='{self.quality}')"
+
+    def test(self):
+        print(f"The table is {self.name}")
+
+table1 = Table("Wood", "GOOD")
+print(table1)
